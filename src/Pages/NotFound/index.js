43e085150d67863bf1d216102f8fe1413c4910e7
mayankitamazons <click4mayank@gmail.com>
2023-06-21 23:56:13 +0530

    doc and not found update

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
 const NotFound = () => {
-  return <h1>NotFound</h1>;
+  return <>
+   <Header />
+   <div style={{marginTop:300,textAlign:"center"}}>
+   <h1>Page Not Exit</h1>
+    </div>
+   <Footer/>
+  
+  </>;
 };
 
 export default NotFound;
